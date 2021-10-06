a9029b097a5c1507148d5cfc56e2a81b399a34a3
Guilherme M Furlaneto <guigogtatropa@gmail.com>
2021-10-06 11:46:07 -0300

    Códigos Discord Monitoria

---
+++ Códigos Discord/Funções/retorno.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct{
+  int resultado;
+}Operacao;
+
+int Soma(int x, int y){
+  return(x+y);
+}//Soma
+
+
+int main(int argc, const char*argv[]){
+  //váriaveis
+  Operacao r;
+  int x=1, y=2;
+
+
+
+  return 0;
+}//Main
